1cdaa62c570e5763a77e52a494c155c01b0d5306
Jason <gt07891@greattree.com.tw>
2022-08-18 17:15:34 +0800

    update: index.js

--- index.js
+++ index.js
@@ -2,22 +2,19 @@ const { yahooNewsCrawler } = require('./services/yahooCrawler');
 const { appendSheet, batchUpdateSheet } = require('./services/googleSheets');
 const { lineNotify } = require('./services/lineNotify');
 const dayjs = require('dayjs');
-const CronJob = require('cron').CronJob;
 const shortUrl = require('node-url-shortener');
 
-require('dotenv').config();
-
-async function crawlerToSheet() {
+async function main() {
   console.log('----crawler start----');
   const result = await yahooNewsCrawler();
   const rows = result.map((item) =>
     Object.values({ 0: item.time, 1: item.title, 2: item.href })
   );
 
-  // console.log('----appendSheet start----');
-  // let sheetName = dayjs().format('YYYY-MM-DD hh mm'); //不能有dash符號
-  // await batchUpdateSheet(sheetName);
-  // await appendSheet(rows, sheetName);
+  console.log('----appendSheet start----');
+  let sheetName = dayjs().format('YYYY-MM-DD hh mm'); //不能有dash符號
+  await batchUpdateSheet(sheetName);
+  await appendSheet(rows, sheetName);
 
   console.log('----notify start----');
   let promiseArr = [];
@@ -46,14 +43,4 @@ async function crawlerToSheet() {
   });
 }
 
-crawlerToSheet();
-
-// new CronJob({
-//   cronTime: process.env.CRONJOB_TIME,
-//   onTick: async function () {
-//     console.log(`開始執行爬蟲排程作業： ${new Date()}`);
-//     await crawlerToSheet();
-//   },
-//   start: true,
-//   timeZone: 'Asia/Taipei',
-// });
+main();
