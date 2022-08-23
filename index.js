0268c166087a1da26559dbc7d921c04c299f735f
Jason <gt07891@greattree.com.tw>
2022-08-23 16:40:30 +0800

    back

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const { lineNotify } = require('./services/lineNotify');
 const { startCron } = require('./services/cron');
 const dayjs = require('dayjs');
 
+const express = require('express');
+const app = express();
+
+var isCronBeenCall = false;
 
 async function main() {
   console.log('----crawler start----');
@@ -18,13 +22,13 @@ async function main() {
     Object.values({ 0: item.time, 1: item.title, 2: item.href })
   );
 
-  // console.log('----appendSheet start----');
-  // await initGoogle();
-  // let sheetName = dayjs().format('YYYYMMDDhhmm'); //表單名稱不能有特殊符號
-  // await addSheet(sheetName);
-  // await appendSheet(rows, sheetName);
-  // let sheetInfo = await getSheetsInfo();
-  // await updateSheetProperties(sheetInfo[sheetInfo.length - 1]);
+  console.log('----appendSheet start----');
+  await initGoogle();
+  let sheetName = dayjs().format('YYYYMMDDhhmm'); //表單名稱不能有特殊符號
+  await addSheet(sheetName);
+  await appendSheet(rows, sheetName);
+  let sheetInfo = await getSheetsInfo();
+  await updateSheetProperties(sheetInfo[sheetInfo.length - 1]);
 
   console.log('----notify start----');
   lineNotify(`排程作業執行完畢！ ${dayjs().format('YYYY-MM-DD hh:mm')}\n${result
@@ -37,5 +41,14 @@ async function main() {
   });
 }
 
-startCron(main);
+app.get('/startCron', async (req, res) => {
+  if (!isCronBeenCall) {
+    startCron(main);
+    isCronBeenCall = true;
+  }
+  res.send(`startCron~~~~ ${isCronBeenCall}`);
+});
 
+app.listen(3030, () => {
+  console.log('app is listening at 3030');
+});
