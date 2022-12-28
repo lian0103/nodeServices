23113fdf0c988e2971ca95731d42e32b987f3958
Jason <gt07891@greattree.com.tw>
2022-12-28 09:34:28 +0800

    update: try catch in page crawlering

--- startJobsCrawler.js
+++ startJobsCrawler.js
@@ -9,15 +9,19 @@ const { writeToDB } = require('./jobs/writeToDB');
 
 startCron(async () => {
     console.log('啟動排程');
-    await jobsCrawler();
-    const file = fs.readFileSync(resolve(__dirname, `./jobs/data.json`), 'utf-8');
-    const fileObject = JSON.parse(file);
-    const msg = `爬取104最新資料共${Object.keys(fileObject).length}筆。更新時間:${dayjs().format(
-        'YYYY-MM-DD hh:mm:ss'
-    )}`;
-    lineNotify(msg);
-    flatJSON();
-    writeToDB();
+    try {
+        await jobsCrawler();
+        const file = fs.readFileSync(resolve(__dirname, `./jobs/data.json`), 'utf-8');
+        const fileObject = JSON.parse(file);
+        const msg = `爬取104最新資料共${Object.keys(fileObject).length}筆。更新時間:${dayjs().format(
+            'YYYY-MM-DD hh:mm:ss'
+        )}`;
+        lineNotify(msg);
+        flatJSON();
+        writeToDB();
+    } catch (error) {
+        lineNotify('error:', error);
+    }
 }, '0 30 8,21 * * *');
 
 // async function run() {
