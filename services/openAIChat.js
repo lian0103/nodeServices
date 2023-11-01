4e98a60a6d34d66939acf046bc1a22ef43e22d6f
Jason Lien <43362355+lian0103@users.noreply.github.com>
2023-11-01 17:06:05 +0800

    Create openAIChat.js

---
+++ services/openAIChat.js
@@ -0,0 +1,46 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import https from "https";
+import fetch from "node-fetch";
+
+const url = "https://api.openai.com/v1/chat/completions";
+
+const customHeaders = {
+  Authorization: `Bearer ${process.env.OPENAI_API_KEY}`, // 添加自定义标头
+  "Content-Type": "application/json",
+};
+
+export const fetchOpenAiChat = async (requestMsg) => {
+  if (!requestMsg) return false;
+
+  const requestData = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: requestMsg }],
+    temperature: 0.7,
+  };
+  return await fetch(url, {
+    method: "POST",
+    headers: customHeaders,
+    agent: new https.Agent({ rejectUnauthorized: false }),
+    body: JSON.stringify(requestData),
+  })
+    .then(async (response) => {
+      return response.arrayBuffer();
+    })
+    .then((Uint8Contents) => {
+      // console.log(Uint8Contents);
+      const textDecoder = new TextDecoder("utf-8");
+      const jsonString = textDecoder.decode(Uint8Contents);
+      const jsonObject = JSON.parse(jsonString);
+      // console.log(jsonObject);
+      // console.log(jsonObject.choices[0].message);
+      return jsonObject.choices[0].message;
+    })
+    .catch(() => {
+      console.error("fetchOpenAiChat error");
+    });
+};
+
+// test
+// let res = await fetchOpenAiChat("你是誰?");
+// console.log(res);
